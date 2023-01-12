4f49d037c5820fdb695b3966884683cac197540e
Jiang Cheng Liu <jiangcl@bu.edu>
2023-01-12 16:25:42 -0500

    deleted smtp.ehlo()

--- emaill.py
+++ emaill.py
@@ -6,9 +6,7 @@ password = config.PASSWORD
 rec = config.RECEIVE_USER
 
 with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-    smtp.ehlo()
     smtp.starttls()
-    smtp.ehlo()
     smtp.login(user, password)
 
     msg = 'hello'
