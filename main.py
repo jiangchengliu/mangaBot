bd78ed62e8b4855fda3b9a7c7df3c39e5ed5b67d
Jiang Cheng Liu <jiangcl@bu.edu>
2023-01-12 11:09:53 -0500

    testing getting urls within favoriteManga tracker

--- main.py
+++ main.py
@@ -5,16 +5,20 @@ rBot = config.create()
 
 subreddit = rBot.subreddit('manga')
 
-
 favoriteMangas = ['legend of the northern blade', 'one punch man']
+
+urls = []
 
 for submission in subreddit.hot(limit=10):
     for manga in favoriteMangas:
         if manga in submission.title.lower():
-            print('Title: ' + submission.title)
-            print('URL: ' + submission.url)
-            print('---------------------------')
-            break
+            if '[DISC]' in submission.title:
+                print('Title: ' + submission.title)
+                urls.append(submission.url)
+                break
+
+for url in urls:
+    print(url)
 
 
     
