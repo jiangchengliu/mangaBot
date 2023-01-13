b94d267fecc76bba4195f03dbbdfcd28955234ed
Jiang Cheng Liu <jiangcl@bu.edu>
2023-01-13 00:37:22 -0500

    fixed circular and use email lib

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import praw
 import config
-import emaill
+import smtplib
+from email.message import EmailMessage
+
+#email info
+user = config.EMAIL_USER
+password = config.PASSWORD
+rec = config.RECEIVE_USER
 
 rBot = config.create()
 subreddit = rBot.subreddit('manga')
-
 favoriteMangas = ['legend of the northern blade', 'one punch man', 'boku no kokoro no yabai yatsu']
-
 urls = []
 
 for submission in subreddit.hot(limit=10):
@@ -14,17 +18,21 @@ for submission in subreddit.hot(limit=10):
         if manga in submission.title.lower():
             if '[DISC]' in submission.title:
                 print('Title: ' + submission.title)
+                print('URL: ' + submission.url)
                 urls.append(submission.url)
                 break
 
+body = "List of Urls: \n"
+for url in urls:
+    body += url + "\n"
 
+msg = EmailMessage()
+msg['Subject'] = 'New Manga Chapters Found!'
+msg['From'] = user
+msg['To'] = rec
+msg.set_content(body)
 
-
-
-
-
-
-
-
-
-    
+with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+    smtp.starttls()
+    smtp.login(user, password)
+    smtp.send_message(msg)
