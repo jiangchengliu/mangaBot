db86ff311a20a35f436ca96bd5eebd183a8791e9
Jiang Cheng Liu <jiangcl@bu.edu>
2023-01-13 00:36:40 -0500

    used email lib and move emaill code-fixed circular

--- emaill.py
+++ emaill.py
@@ -1,14 +1,14 @@
 import smtplib
 import config
-import main
 
 user = config.EMAIL_USER
 password = config.PASSWORD
 rec = config.RECEIVE_USER
 
-def send_email():
+def send_email(list):
     with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
         smtp.starttls()
         smtp.login(user, password)
-        for url in main.urls:
+        for url in list: 
             smtp.sendmail(user, rec, url)
+            
