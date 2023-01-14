91e90b3807d8ba23fd1ff88a34cf8accb277cb94
Jiang Cheng Liu <jiangcl@bu.edu>
2023-01-14 17:02:56 -0500

    import threading + made goThroughCom func

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import praw
 import config
 import eMail
+import threading 
 
 #initialize bot related variables
 rBot = config.create()
@@ -19,19 +20,18 @@ for submission in subreddit.hot(limit=25):
                 if submission.title not in my_dict:
                     my_dict[submission.title] = submission.url
                     break
-"""
-#go through comments
-for comment in subreddit.stream.comments():
-    if "add" in comment.body:
-        text = comment.body
-        start = text.index("add") + 4
-        if "!" in comment.body:
-            end = text.index("!")
-            title = text[start:end].strip()
-            if title not in my_list:
-                my_list.append(title)
-                break
-"""
+def goThroughCom():
+    #go through comments
+    for comment in subreddit.stream.comments():
+        if "add" in comment.body:
+            text = comment.body
+            start = text.index("add") + 4
+            if "!" in comment.body:
+                end = text.index("!")
+                title = text[start:end].strip()
+                if title not in my_list:
+                    my_list.append(title)
+                    break
 
 body = eMail.create_body(my_dict)
 msg = eMail.set_mail(body)
