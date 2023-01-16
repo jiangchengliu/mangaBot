b45107c900df274c819b1877ab369a008b078551
Jiang Cheng Liu <jiangcl@bu.edu>
2023-01-16 00:43:31 -0500

    comment loop semi works - variable lists not

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ import threading
 rBot = config.create()
 subreddit = rBot.subreddit('manga')
 sub = rBot.subreddit('testingMyStuffCode')
-my_list = ["ayakashi triangle", "cockroa-chan"]
+my_list = ["One Piece", "Legend of the Northern Blade", "Jujutsu Kaisen", "Ayakashi Triangle"]
 favoriteMangas = list(map(str.lower, my_list))
 title = ""
 my_dict = {}
-
+processed_comments = []
 
 #go through submissions 
 for submission in subreddit.hot(limit=25):
@@ -22,31 +22,32 @@ for submission in subreddit.hot(limit=25):
                     my_dict[submission.title] = submission.url
                     break
 
-def goThroughCom(sub, list):
+def goThroughCom(sub, list, p_com):
     global my_list
-    for comment in sub.stream.comments():
-        found_add = False
-        if "add" in comment.body:
-            text = comment.body
-            start = text.index("add") + 4
-            if "manga" in comment.body:
-                end = text.index("mangaBot")
-                title = text[start:end].strip()
-                if title not in my_list:
-                    my_list.append(title)
-                    found_add = True
-        if found_add:
-            break
-        
-print(my_list)
+    global processed_comments
+    for comment in sub.stream.comments(skip_existing=True):
+        if comment.id not in processed_comments:
+            if "add" in comment.body:
+                text = comment.body
+                start = text.index("add") + 4
+                if "manga" in comment.body:
+                    end = text.index("mangaBot")
+                    title = text[start:end].strip()
+                    if title not in my_list:
+                        my_list.append(title)
+            processed_comments.append(comment.id)
+        print(my_list)
+        print(processed_comments)
 
-comments_thread = threading.Thread(target=goThroughCom, args=(sub, my_list))
+print(my_list)
+print(processed_comments)
+
+comments_thread = threading.Thread(target=goThroughCom, args=(sub, my_list, processed_comments))
 comments_thread.start()
 
 body = eMail.create_body(my_dict)
 msg = eMail.set_mail(body)
 eMail.send(msg)
-my_dict.clear()
 
 
 
