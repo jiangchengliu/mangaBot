bf049f472f5d5b254f42c1b817f6178c3ebe07d1
Jiang Cheng Liu <jiangcl@bu.edu>
2023-01-19 00:01:28 -0500

    not working - changed favorite funcs and vars

--- main.py
+++ main.py
@@ -5,46 +5,45 @@ import threading
 import time
 
 #initialize bot related variables
-my_list = ["One Punch man", "One Piece", "Ace of Diamond"]
-favoriteMangas = list(map(str.lower, my_list))
-processed_comments = []
-processed_subs = []
+my_list = ["Naruto"]
+favoriteMangas = [title.lower() for title in my_list]
 
-def check_for_updates(submission):
-    if submission.id not in processed_subs:
-        processed_subs.append(submission.id)
-        return True
 
-def favorite_title(comment):
+def favorite_title(sub):
     title = ""
-    if comment.id not in processed_comments:
-        if "add" in comment.body:
-            text = comment.body
-            start = text.index("add") + 4
-            if "manga" in comment.body:
-                end = text.index("manga")
-                title = text[start:end].strip()
-                if title not in my_list:
-                    my_list.append(title)
-                    processed_comments.append(comment.id)  
-    
+    new_list = []
+    processed_comments = []
+    for comment in sub.stream.comments(skip_existing=False):
+        if comment.id not in processed_comments:
+            if "add" in comment.body:
+                text = comment.body
+                start = text.index("add") + 4
+                if "manga" in comment.body:
+                    end = text.index("manga")
+                    title = text[start:end].strip()
+                    if title.lower() not in favoriteMangas:
+                        new_list.append(title.lower())
+                        processed_comments.append(comment.id)
+    favoriteMangas.extend(new_list)
+    print("manga list right now")
+    print(my_list)
+    print("processed comment id")
+    print(processed_comments)
+    return favoriteMangas
+
+
 def main():
     rBot = config.create()
     subreddit = rBot.subreddit('manga')
     sub = rBot.subreddit('testingMyStuffCode')
+    title_list = favorite_title(sub)
     while True:
         for submission in sub.stream.submissions(skip_existing=True):
-            for manga in favoriteMangas:
+            for manga in title_list:
                 if manga in submission.title.lower():
                     if '[DISC]' in submission.title:
-                        if check_for_updates(submission):
-                            body = eMail.create_body(submission, rBot)
-                            msg = eMail.set_mail(body)
-                            eMail.send(msg)
-                for comment in submission.comments.list():
-                    favorite_title(comment)
-        for comment in sub.stream.comments(skip_existing=True):
-                favorite_title(comment)
+                        body = eMail.create_body(submission, rBot)
+                        msg = eMail.set_mail(body)
+                        eMail.send(msg)
         
-
 main()
